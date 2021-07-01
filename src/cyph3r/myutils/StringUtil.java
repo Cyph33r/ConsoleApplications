670b291815f84012057d3867dc45c38f3517b7c8
Tade Ajai <tadeajai4@gmail.com>
2021-07-01 11:18:12 +0100

    Updated the StringUtil class.
    Signed-off-by: Tade Ajai <tadeajai4@gmail.com>

--- src/cyph3r/myutils/StringUtil.java
+++ src/cyph3r/myutils/StringUtil.java
@@ -3,33 +3,77 @@ package cyph3r.myutils;
 import java.util.Random;
 
 public class StringUtil {
-	static Random gen = new Random();
-	static String letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	static String digits = "0123456789";
-
+	final static String asciiUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	final static String whitespace = " \t\n\r\f";
+	final static String digits = "0123456789";
+	final static String hexdigits = digits + "abcdef" + "ABCDEF";
+	final static String octdigits = "01234567";
+	final static String asciiLowercase = "abcdefghijklmnopqrstuvwxyz";
+	final static String punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+	final static String asciiLetters = asciiLowercase + asciiUppercase;
+	final static String printable = digits + asciiLetters + punctuation + whitespace;
+	final static Random gen = new Random();
 
 	public static void main(String[] args) {
-		for (char i = 0; i < 160; ++i) {
-
-//			System.out.println((int) i + " -> " + i);
-			System.out.println((StringUtil.getRandomLetter()));
-
-
-		}
+		System.out.println(toTitlecase("fg wh thr why"));
+//		System.out.println(Character.toTitleCase('s'));
 	}
 
-	public static char getRandomLetter() {
-		return (letters + digits).charAt(gen.nextInt(52));
+	public static String toTitlecase(String word) {
+		StringBuilder toReturn = new StringBuilder();
+		boolean seenWhitespace = true;
+		for (int i = 0; i < word.length(); i++) {
+			char character = word.charAt(i);
+			if (!(isWhitespace(character))) {
+				if (seenWhitespace)
+					toReturn.append(Character.toTitleCase(character));
+				else
+					toReturn.append(character);
+				seenWhitespace = false;
 
+				continue;
+			}
+
+			seenWhitespace = true;
+			toReturn.append(character);
+		}
+		return toReturn.toString();
+	}
+
+	public static boolean isWhitespace(String sequence) {
+		if (sequence.isEmpty())
+			return false;
+		for (int i = 0; i < sequence.length(); i++) {
+			char character = sequence.charAt(i);
+			for (int j = 0; j < whitespace.length(); j++)
+				if (!(Character.isWhitespace(character)))
+					return false;
+		}
+		return true;
+
+
+	}
+
+	public static boolean isWhitespace(char character) {
+		return isWhitespace(Character.toString(character));
+	}
+
+	public static char getRandomLetterAsChar() {
+		return (asciiLetters + digits).charAt(gen.nextInt(52));
+
+	}
+
+	public static String getRandomLetterAsString() {
+		return Character.toString(getRandomLetterAsChar());
 	}
 
 	public static String getRandomString(int length) {
 		StringBuilder string = new StringBuilder();
 		for (int i = 0; i < length; i++)
-			string.append(getRandomLetter());
-
+			string.append(getRandomLetterAsChar());
 		return string.toString();
 	}
+
 
 }
 
